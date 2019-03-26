95e5051c720aa1e08923c15530acf76bfcb5f10a
kawa <rodrigo@kawa>
2019-03-25 23:18:45 -0300

    suma funciones 2

--- suma numeros clase funcionesw/main.c
+++ suma numeros clase funcionesw/main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-  int suma(int op1,int op2){
-    int res;
-    res=op1+op2;
-    return res;
-    }
+int suma(int op1 ,int op2 );// prototip no es necesario q declare las variables con solo el tipo de variable basta o directamente
+//con toda la ejecucion no varia al principio sin el prototipo  parametro formal estrictamente riguroso le paso 2 enteros
 int main()
 
 {
@@ -15,7 +12,12 @@ int main()
     scanf("%d",&n1);
     printf("numero2");
     scanf("%d",&n2);
-    result=suma(n1,n2);
+    result=suma(n1,n2);// llamada a la funcionc
     printf("la suma es :%d",result);
     return 0;
 }
+ int suma(int op1,int op2){//cuerpo desarrollo q hace la funcion tambien aca esta el partametro formal estrictamente riguroso
+    int res;
+    res=op1+op2;
+    return res;
+    }
