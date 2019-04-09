c541e639ef44c715777e466db394aeefd8debff3
alumno <alumno>
2019-04-08 22:00:36 -0300

    concatenar caracteres

--- cadenas de caracteres clase 6/comparar cadenas de caracteres (contenido )/main.c
+++ cadenas de caracteres clase 6/comparar cadenas de caracteres (contenido )/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
  /**
   usamos las funcion :
    strcmp (char *, char *)
@@ -9,17 +10,24 @@
    char  > char devuelve
 
  */
-int main()
-{   char apellido[12];
-    char nombre[8];
+void main(void)
+{   char apellido[20];
+    char nombre[20];
+    char apellidonombre[60];
       printf("ingrese apellido \n luego nombre \n");
       fflush(stdin);
       scanf("%s",apellido);
       fflush(stdin);
       scanf("%s",nombre);
+    strlwr(apellido);
+    apellido[0]=toupper(apellido[0]);/** para pasar a mayuscula la primer letra de la variable */
+    strlwr(nombre);
+    nombre[0]=toupper(nombre[0]);
+   strcpy(apellidonombre,apellido);
+    strcat(apellidonombre,apellido);
+    strcat(apellido," ");
+     strcat(apellidonombre,nombre);
+    printf("%s",apellidonombre);
 
-    strcat(apellido,nombre);
-    printf("%s",apellido);
 
-return ;
 }
