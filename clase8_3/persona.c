5aebbc84addb23ef7769d0094b9f5236d57c150b
alumno <alumno@utnfra.com.ar>
2019-04-16 21:50:58 -0300

    clase 9

--- clase8_3/persona.c
+++ clase8_3/persona.c
@@ -33,3 +33,20 @@ void mostrarTodos( ePersona pers[], int cantidad ){
 void mostrarUno( ePersona per ){
         printf("%s\t%s\t%d\t%d\n", per.apellido, per.nombre, per.edad, per.isEmpty);
 }
+void  ordenar (ePersona per[],int cantidad){
+        int i,j;
+        ePersona aux;
+        for(i=0;i<cantidad-1;i++){
+                for(j=i+1;i<cantidad;j++){
+            if(strcmp(per[i].apellido,per[j].apellido)>0){
+                aux=per[i];
+                per[i]=per[j];
+                per[j]=aux;
+            }
+        }
+    }
+
+
+}
+
+eNacion nacionalidad[]={1,"argentina",2,"peru",3."uruguay"};
--- clase8_3/persona.h
+++ clase8_3/persona.h
@@ -1,12 +1,21 @@
 typedef struct{
+int id;
+char descripcion[31];
+}eNacion;
+
+
+typedef struct{
     char apellido[31];
     char nombre[31];
     int edad;
     int isEmpty;
+    int idNacionalidad;
 }ePersona;
 
 void alta( ePersona pers[], int cantidad );
 
 void mostrarUno( ePersona per );
 
-void mostrarTodos( ePersona pers[], int cantidad );
+void mostrarTodos( ePersona pers[], int cantidad);
+
+void  ordenar (ePersona per[],int cantidad);
